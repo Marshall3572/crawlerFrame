bd75708a4274544fbd20b4678d83e4b66f40b811
Marshall3572 <944797458@qq.com>
2022-04-08 12:01:04 +0800

    feat: 实现 postman 收发请求，添加&查询 users list

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const users = []
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+    .get(function (req, res, next) {
+        res.json(users)
+    })
+    .post((req, res) => {
+        const user = req.body
+        users.push(user)
+        res.json(user)
+    })
 
 module.exports = router;
