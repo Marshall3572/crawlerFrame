b8c5765d00eddafddd535adc6dc6d55a6ff37a3c
Marshall3572 <944797458@qq.com>
2022-04-21 11:54:22 +0800

    feat: add logger to mongodb_connnections

--- services/mongodb_connnections.js
+++ services/mongodb_connnections.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose')
+const logger = require('../utils/loggers/logger')
+
 mongoose.Promise = Promise
 
+// todo
 const uri = 'mongodb://127.0.0.1:27018/what_i_love'
-mongoose.connect(uri,{
+/*mongoose.connect(uri,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-const db = mongoose.connection
+const db = mongoose.connection*/
+mongoose.connect(uri, { useMongoClient: true });
+const db = mongoose.connection;
 
 db.on('open', () => {
-    console.log('db connected!')
+    logger.info(`successfully connecting to db, uri: ${uri}`);
 })
 
 db.on('error', (e) => {
-    console.log(e)
+    logger.error(`error connecting to db, uri: ${uri}`, { err: e });
 })
 
 module.exports = db
